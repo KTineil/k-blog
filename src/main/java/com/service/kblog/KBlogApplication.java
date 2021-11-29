95fc3594f9015efd251b0ceea5345be6282aba09
KHW <gurdnrnjs19@gmail.com>
2021-11-30 01:17:41 +0900

    first commit

---
+++ src/main/java/com/service/kblog/KBlogApplication.java
@@ -0,0 +1,13 @@
+package com.service.kblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KBlogApplication.class, args);
+	}
+
+}
