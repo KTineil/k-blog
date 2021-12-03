c6466cbf1b8f0f3eee50217791c24b449ebb01aa
KHW <gurdnrnjs19@gmail.com>
2021-12-03 22:17:25 +0900

    webapp/WEB-INF/views 디렉토리를 jsp 파일 저장용 디렉토리로 설정 spring boot에서 jsp와 jstl을 이해할 수 있도록 dependency 설정 application.properties -> application.yml 파일로 변경
    index.jsp와 blog.jsp  파일에 접근할수 있도록 Testcontroller 생성

--- src/main/java/com/service/kblog/controller/TestController.java
+++ src/main/java/com/service/kblog/controller/TestController.java
@@ -1,23 +1,20 @@
 package com.service.kblog.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-import com.service.kblog.dto.TestRequestBodyDTO;
-
-@RestController
-@RequestMapping("/test")
+@Controller
+@RequestMapping("/")
 public class TestController {
-	@GetMapping("/testRequestBody")
-	public String testController(@RequestBody TestRequestBodyDTO testRequestBodyDTO)  {
-		return "Hello Get " + testRequestBodyDTO.getId() + " " + testRequestBodyDTO.getMessage();
+	
+	@GetMapping()
+	public String testIndexController() {
+		return "index";
 	}
 	
-	@GetMapping("/testRequestParam")
-	public String testController(@RequestParam(required = false) int id) {
-		return "Hello Get Id " + id;
+	@GetMapping("/blog")
+	public String testBlogController() {
+		return "blog";
 	}
 }
