b8f754d8599b6a65f79e7026b97fa4827bb8c33c
tineil_macOS <gurdnrnjs19@gmail.com>
2022-01-23 03:25:36 +0900

    - pagination 로직 수정 	현제 페이지가 페이지범위 중간에 위치하도록 변경

--- src/main/java/com/service/kblog/util/PageVO.java
+++ src/main/java/com/service/kblog/util/PageVO.java
@@ -36,22 +36,33 @@ public class PageVO {
 	
 	public void pagination(int cntOfPosts) {
 		this.lastPage = cntOfPosts != 0 ? (int) Math.ceil( (double) cntOfPosts / 5) : 1;
-		setStartPageOfrange();
+		setStartPageOfRange();
 		setEndPageOfRange();
 		this.limitStartIndex = (page-1) * size;
 		this.prevPage = page == 1 ? -1 : page - 1;
 		this.nextPage = page == lastPage ? -1 : page + 1;
 	}
 	
-	public void setStartPageOfrange() {
-		if (lastPage - 4 <= 0) {
+	public void setStartPageOfRange() {
+		
+		// 마지막 페이지가 페이지범위에 있으면 시작 페이지 고정 
+		if (page + 2 >= lastPage) {
+			if (lastPage <= 5) {
+				startPageOfRange = 1;
+			}
+			else {
+				startPageOfRange = lastPage - 4;
+			}
+		}
+	
+		// 3보다 페이지가 작으면 범위 넘어감 효과X
+		else if (page < 3) {
 			startPageOfRange = 1;
 		}
-		else if (lastPage - 4 <= page) {
-			startPageOfRange = lastPage - 4;
-		}
+		
+		// 두 예 경우를 제외하고 일반적인 페이지 넘김 처
 		else {
-			startPageOfRange = page;
+			startPageOfRange = page - 2;
 		}
 	}
 	
